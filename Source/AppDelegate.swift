813d554367957429bad8d6eacf545e5e8b8c0f91
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-02-11 18:23:43 -0500

    Add files

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  LoginView
+//
+//  Created by Jeff Kereakoglow on 2/11/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
+
---
+++ Source/LoginController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginController.swift
+//  LoginView
+//
+//  Created by Jeff Kereakoglow on 2/11/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UITableViewController {
+  @IBOutlet weak var usernameField: UITextField!
+  @IBOutlet weak var passwordField: UITextField!
+}
+
+// MARK: - Navigation
+extension LoginController {
+  override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+    guard identifier == "login" else {
+      return false
+    }
+
+    return true
+  }
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginController: UITextFieldDelegate {
+  func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+    return true
+  }
+
+  // Dismiss the keyboard when the user hits the return key
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+
+    return true
+  }
+}
